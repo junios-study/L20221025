c83181bf268431ad4783c12bb3b92a3aadc5ecb3
junios <junios@naver.com>
2022-10-25 16:14:24 +0900

    입력 처리 생성

--- Source/L20221025/MyPawn.cpp
+++ Source/L20221025/MyPawn.cpp
@@ -10,11 +10,12 @@
 #include "GameFramework/FloatingPawnMovement.h"
 #include "Engine/StaticMesh.h"
 #include "Components/ArrowComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AMyPawn::AMyPawn()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	Box = CreateDefaultSubobject<UBoxComponent>(TEXT("Box"));
@@ -39,7 +40,7 @@ AMyPawn::AMyPawn()
 	{
 		Right->SetStaticMesh(SM_Propeller.Object);
 		Left->SetStaticMesh(SM_Propeller.Object);
-		Right->SetRelativeLocation(FVector(36.269119f, 21.026630f,0.310581f));
+		Right->SetRelativeLocation(FVector(36.269119f, 21.026630f, 0.310581f));
 		Left->SetRelativeLocation(FVector(36.269119f, -21.026630f, 0.310581f));
 	}
 
@@ -57,16 +58,14 @@ AMyPawn::AMyPawn()
 	Arrow->SetRelativeLocation(FVector(80.f, 0, 0));
 
 	Movement = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("Movement"));
-	Movement->MaxSpeed = 100.0f;
-
-
+	Movement->MaxSpeed = MoveSpeed;
 }
 
 // Called when the game starts or when spawned
 void AMyPawn::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
@@ -74,8 +73,8 @@ void AMyPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	Left->AddLocalRotation(FRotator(0, 0, 3600.0f * DeltaTime));
-	Right->AddLocalRotation(FRotator(0, 0, 3600.0f * DeltaTime));
+	Left->AddLocalRotation(FRotator(0, 0, PropellerRoationSpeed * DeltaTime));
+	Right->AddLocalRotation(FRotator(0, 0, PropellerRoationSpeed * DeltaTime));
 	//AddActorLocalOffset(FVector(100.0f * DeltaTime, 0, 0));
 
 	AddMovementInput(GetActorForwardVector());
@@ -86,5 +85,31 @@ void AMyPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAction(TEXT("Fire"), IE_Pressed, this, &AMyPawn::Fire);
+	PlayerInputComponent->BindAxis(TEXT("Pitch"), this, &AMyPawn::Pitch);
+	PlayerInputComponent->BindAxis(TEXT("Roll"), this, &AMyPawn::Roll);
+}
+
+void AMyPawn::Fire()
+{
+	GetWorld()->SpawnActor<AActor>(AActor::StaticClass(),
+		GetActorLocation(),
+		GetActorRotation());
+}
+
+void AMyPawn::Pitch(float Value)
+{
+	AddActorLocalRotation(FRotator(Value * RotationSpeed *
+		UGameplayStatics::GetWorldDeltaSeconds(GetWorld()),
+		0,
+		0
+	));
+}
+
+void AMyPawn::Roll(float Value)
+{
+	AddActorLocalRotation(FRotator(0, 0, Value * RotationSpeed *
+		UGameplayStatics::GetWorldDeltaSeconds(GetWorld())
+	));
 }
 
--- Source/L20221025/MyPawn.h
+++ Source/L20221025/MyPawn.h
@@ -34,19 +34,42 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	void Fire();
+
+	void Pitch(float Value);
+
+	void Roll(float Value);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
 	UBoxComponent* Box;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	UStaticMeshComponent* Body;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	UStaticMeshComponent* Left;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	UStaticMeshComponent* Right;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	USpringArmComponent* SpringArm;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	UCameraComponent* Camera;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	UFloatingPawnMovement* Movement;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	UArrowComponent* Arrow;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Speed")
+	float RotationSpeed = 60.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Speed")
+	float PropellerRoationSpeed = 3600.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Speed")
+	float MoveSpeed = 100.0f;
 };
