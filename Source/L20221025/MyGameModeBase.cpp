9b7aeb5644edec8a933de9a0c795477724b50a39
junios <junios@naver.com>
2022-10-25 11:31:13 +0900

    C++ 클래스 만들기

---
+++ Source/L20221025/MyGameModeBase.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameModeBase.h"
+
+AMyGameModeBase::AMyGameModeBase()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Hello World!"));
+}
